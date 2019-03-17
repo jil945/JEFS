14fadc821207796ad61e0b84ec3aee2f0646285c
Jim Lee <jimleee945@gmail.com>
2019-03-16 18:24:52 -0700

    Remove placeholder recipe/workouts

--- src/util/http.js
+++ src/util/http.js
@@ -13,56 +13,10 @@ const httpRecipe = axios.create({
         "X-RapidAPI-Key": RECIPE_KEY
     }
 });
-
-// PLACEHOLDER results
-const MEAL = {
-    "glutenFree" : false,
-    "ingredients" : [
-        "arugula",
-        "baguette",
-        "basil",
-        "butter",
-        "canned tomatoes",
-        "crushed red pepper",
-        "eggs",
-        "garlic cloves",
-        "olive oil",
-        "onion",
-        "parmesan cheese",
-        "rosemary",
-        "salt and pepper",
-        "thyme"
-    ],
-    "id" : 640636,
-    "title" : "Creamy Egg Marinara Breakfast Dip",
-    "cuisines" : [],
-    "vegan" : false,
-    "image" : "https://spoonacular.com/recipeImages/640636-556x370.jpg",
-    "vegetarian" : false
-};
-const WEEKDAYS = ["mondy", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-const _GET_RECIPIE = WEEKDAYS.reduce((a, w) => {
-    a[w] = {
-        "breakfast": MEAL,
-        "lunch": MEAL,
-        "dinner": MEAL,
-    };
-    return a;
-}, {});
-const GET_RECIPIE = Promise.resolve(_GET_RECIPIE);
-const WORKOUT = {
-    name: "Bench Press",
-    description: "A bench press",
-    picture: "https://weighttraining.guide/wp-content/uploads/2016/10/Incline-Reverse-Grip-Barbell-Bench-Press.jpg"
-};
-const _GET_WORKOUT = WEEKDAYS.reduce((a, w) => {
-    a[w] = [WORKOUT];
-    return a;
-}, {});
-const GET_WORKOUT = Promise.resolve(_GET_WORKOUT);
+const WEEKDAYS = ["sunday", "mondy", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 const http  = axios.create({
     baseURL: BASE_URL
 });
-export { BASE_URL, GET_WORKOUT, GET_RECIPIE, httpRecipe };
+export { BASE_URL, httpRecipe, WEEKDAYS };
 export default http;
